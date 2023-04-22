194eb1407374bbe193185280cf5a3ebdea94ac90
= <=>
2023-04-22 19:08:10 +0100

    modified 3 files

--- src/Components/header/LiNavLinkComp.tsx
+++ src/Components/header/LiNavLinkComp.tsx
@@ -1,7 +1,16 @@
-import { NavLink } from "react-router-dom"
-import activeStyle from "../../ts/activeStyle"
-import { NavLinkProps } from "../../ts/customTypes"
+import { NavLink } from "react-router-dom";
+import activeStyle from "../../ts/activeStyle";
+import { NavLinkProps } from "../../ts/customTypes";
 
-export default function LiNavLinkComp({ path, children, ...rest }: NavLinkProps) {
-  return <li {...rest}><NavLink style={({ isActive }) => isActive ? activeStyle : undefined} to={path}>{children}</NavLink></li>
-}
+const LiNavLinkComp = ({ path, children, ...rest }: NavLinkProps) => (
+  <li className="md:py-2 md:text-[1.05rem]" {...rest}>
+    <NavLink
+      style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      to={path}
+    >
+      {children}
+    </NavLink>
+  </li>
+);
+
+export default LiNavLinkComp;
