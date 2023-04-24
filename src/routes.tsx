d578868f665734a52d3d06075a4ebad2feb70821
= <=>
2023-04-24 19:15:06 +0100

    modified routes.tsx added Collections.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,7 @@
-import {
-    Route,
-    createRoutesFromElements
-} from "react-router-dom"
+import { Route, createRoutesFromElements } from "react-router-dom";
 
 import Layout from "./Components/Layout";
+import Collections from "./Components/Collections";
 
 import Home from "./Pages/Home";
 import About from "./Pages/About";
@@ -12,16 +10,20 @@ import Blog from "./Pages/Blog";
 import NotFound from "./Pages/NotFound";
 
 const routes = createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="architecture-website" element={<Home />} />
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
+    <Route path="architecture-website" element={<Home />} />
 
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="blog" element={<Blog />} />
+    <Route path="about" element={<About />} />
+    <Route path="contact" element={<Contact />} />
+    <Route path="blog" element={<Blog />} />
 
-        <Route path="*" element={<NotFound />} />
+    <Route path="collections" element={<Collections />}>
+      <Route />
     </Route>
-)
 
-export default routes
+    <Route path="*" element={<NotFound />} />
+  </Route>
+);
+
+export default routes;
