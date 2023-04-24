08e44bf09cfadf55cb4a1a3fc100b82a0df82795
= <=>
2023-04-24 12:17:44 +0100

    modified WelcomeComp.tsx in home

--- src/Components/home/WelcomeComp.tsx
+++ src/Components/home/WelcomeComp.tsx
@@ -2,7 +2,7 @@ import { WelcomeCompProps } from '../../ts/customTypes'
 
 const WelcomeComp = ({ heading, text, ...rest }: WelcomeCompProps) => (
     <div {...rest}>
-        <h1>{heading}</h1>
+        <h1 className='h1'>{heading}</h1>
         <p>{text}</p>
     </div>
 )
