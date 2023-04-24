8e3b2b402b0029e8e8e91cf35b3f9d268291c72b
= <=>
2023-04-24 19:35:08 +0100

    modified NotFound.tsx in Pages

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,5 +1,14 @@
+import useWindowTitle from "../hooks/useWindowTitle";
+
 export default function NotFound() {
-    return (
-        <div className="min-h-screen flex items-center justify-center">Not Found</div>
-    )
+  useWindowTitle("NotFound | Architecture.com");
+  
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-start pt-8 p-4">
+      <h1>
+        <strong className="text-[10rem] md:text-[16rem]">404</strong>
+      </h1>
+      <p className="text-xl md:text-3xl text-gray-light">Seems the page you're looking for does not exist</p>
+    </div>
+  );
 }
