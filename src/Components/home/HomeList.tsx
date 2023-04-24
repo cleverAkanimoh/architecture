39e62f872b63fefa6af67cdc4897e92613d7ed95
= <=>
2023-04-24 12:12:54 +0100

    modified 3 files

--- src/Components/home/HomeList.tsx
+++ src/Components/home/HomeList.tsx
@@ -5,7 +5,7 @@ const HomeList = ({ text, underline }: HomeListProps) => (
   <p
     className={`${
       underline ? "border-b border-gray-dim" : ""
-    } flex items-center text-lg py-3`}
+    } flex items-center text-lg py-3 md:py-2`}
   >
     <VscPassFilled className="mr-1 text-yellow-300" />
     {text}
