68e27957788f4c3120bfeb2391d6d71638e06a89
= <=>
2023-05-13 20:05:57 +0100

    modified Footer.tsx in Components

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,10 +4,11 @@ import { HomePlansLink } from "./home/HomePlansComp";
 
 export default function Footer() {
   const year = new Date().getFullYear();
-  
+
   return (
     <footer className="mt-14 pb-5 w-full capitalize flex flex-col items-center justify-center border-t-2 border-gray-dim">
-      <section className="p-3 my-14 m-w-[300px] w-[99%] max-w-[1300px] grid md:grid-cols-2 lg:grid-cols-3 xl:flex xl:justify-between gap-3">
+      <section className="p-3 my-14 m-w-[300px] w-[99%] max-w-[1300px] grid md:grid-cols-2 lg:grid-cols-3 xl:flex xl:justify-between xl:items-start gap-3">
+
         <div className="uppercase flex items-center bg-orange-400 rounded-lg w-2/3 max-w-[330px] h-[120px]">
           <h1 className="h1 text-white animate-pulse">logo here</h1>
         </div>
@@ -42,22 +43,22 @@ export default function Footer() {
 
         <div className="lowercase my-5">
           <H3 text="subscribe to our newsletter!" />
-          <Form className="my-4 flex hover:border-l-4 border-orange-500 p-4 md:flex-row flex-col">
+          <Form className="my-4 flex p-4 md:p-0 md:flex-row flex-col">
             <input
               type="email"
-              className="w-full rounded-none px-2 h-9 outline-none xl:rounded-l-full text-gray-dark"
+              className="w-full rounded-none px-2 h-9 font-sofia outline-none text-gray-dark"
               placeholder="Email"
             />
-            <button className="capitalize bg-blue-500 hover:bg-opacity-80 text-white w-full rounded-none mt-3 md:mt-0 h-9 xl:rounded-r-full">
+            <button className="capitalize bg-blue-500 hover:bg-opacity-80 text-white w-full rounded-none mt-3 md:mt-0 h-9">
               join now
             </button>
           </Form>
         </div>
-        
+
       </section>
       <p className="lowercase text-center underline text-sm sm:text-base hover:text-white">
-          Copyright © {year} architecture.com
-        </p>
+        Copyright © {year} architecture.com
+      </p>
     </footer>
   );
 }
