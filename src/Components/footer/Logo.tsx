5a18b35a71ff9c405a0380c771032b97cfa5b59f
= <=>
2023-05-21 11:29:16 +0100

    modified 4 files

--- src/Components/footer/Logo.tsx
+++ src/Components/footer/Logo.tsx
@@ -1,7 +1,7 @@
 import { VscHome } from "react-icons/vsc"
 
 const Logo = () => (
-    <div className="flex flex-col items-center w-full sm2:col-span-3 max-w-[350px] min-h-[130px] text-blue-400 font-extrabold mb-2 sm2:mb-4 self-center xl:self-start border sm:border-none transition-all duration-300">
+    <div className="flex flex-col items-center w-full sm2:col-span-3 max-w-[350px] min-h-[130px] text-blue-400 font-extrabold mb-2 sm2:mb-4 self-center lg:self-start border sm:border-none transition-all duration-300">
 
         <div className="flex items-center border sm2:border-none w-full transition-all duration-300">
             <VscHome className="w-[100px] h-20 sm2:h-24 border md:border-none transition-all duration-300" />
--- src/Components/footer/Subscribe.tsx
+++ src/Components/footer/Subscribe.tsx
@@ -5,14 +5,15 @@ import H3 from './H3'
 const Subscribe = () => (
     <div className="lowercase">
         <H3 text="subscribe to our newsletter!" />
-        <Form action="#" className="my-4 flex md:flex-row flex-col">
+        <Form action="subscribe" className="my-4 flex md:flex-row flex-col md:bg-white ">
             <input
                 type="email"
-                className="w-full rounded-none px-2 h-9 font-sofia outline-none text-gray-dark"
+                className="w-full rounded-none px-2 h-9 font-sofia outline-none text-gray-dark md:bg-transparent"
                 placeholder="Email"
             />
-            <button className="capitalize bg-blue-500 hover:bg-opacity-70 text-white w-full rounded mt-3 md:mt-0 h-9 xl:text-sm">
-                join now
+            <button className="lowercase bg-blue-500 text-white w-full md:rounded-l-full mt-3 md:mt-0 md:w-1/2 md:text-[.75rem] h-10 xl:text-sm overflow-hidden relative group">
+                <span>subscribe</span>
+                <span className='bg-black bg-opacity-0 text-[.92rem] w-full h-full absolute top-[-100%] left-0 group-hover:top-0 group-hover:bg-opacity-40 transition-all duration-500 motion-reduce:transition-none'></span>
             </button>
         </Form>
     </div>
